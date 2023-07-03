323a6951a1536bd0de0a5fac1f0c6e25e0605ed3
amohacker <tnvtablet@gmail.com>
2023-07-03 20:34:28 +0200

    reenabled download

--- installeer.bat
+++ installeer.bat
@@ -3,7 +3,7 @@ where node.exe >nul 2>&1 && goto install || goto node
 pause
 :node
 echo Node.js is not installed, downloading now...
-:: powershell -command "wget -Uri 'https://nodejs.org/dist/v18.16.1/node-v18.16.1-x64.msi' -OutFile 'npm-installer.msi'"
+powershell -command "wget -Uri 'https://nodejs.org/dist/v18.16.1/node-v18.16.1-x64.msi' -OutFile 'npm-installer.msi'"
 echo Please follow the opened installer
 start /W npm-installer.msi
 endlocal
