455e641592cdf69e32d322831de91840d6bafd9d
amohacker <tnvtablet@gmail.com>
2023-07-03 20:21:41 +0200

    updated installeer.bat

--- installeer.bat
+++ installeer.bat
@@ -3,13 +3,21 @@ where node.exe >nul 2>&1 && goto install || goto node
 pause
 :node
 echo Node.js is not installed, downloading now...
-powershell -command "wget -Uri 'https://nodejs.org/dist/v18.16.1/node-v18.16.1-x64.msi' -OutFile 'npm-installer.msi'"
+:: powershell -command "wget -Uri 'https://nodejs.org/dist/v18.16.1/node-v18.16.1-x64.msi' -OutFile 'npm-installer.msi'"
 echo Please follow the opened installer
 start /W npm-installer.msi
+endlocal
+call RefreshEnv.cmd
 goto install
 
 :install
 echo updating npm...
-npm install npm@latest
+call npm install npm@latest
 echo Installing and updating packages...
-npm install
+call npm install
+cls
+echo Done!
+SET /P AREYOUSURE=Do you want to start the dashboard now (Y/[N])?
+IF /I "%AREYOUSURE%" EQU "Y" npm run dev
+IF /I "%AREYOUSURE%" EQU "y" npm run dev
+pause
